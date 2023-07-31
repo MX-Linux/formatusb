d0ff43a36709d6075aa8be44445c506ebae48659
Dolphin Oracle <dolphinoracle@gmail.com>
2023-07-31 17:35:14 -0400

    update translations

--- translations/formatusb_tr.ts
+++ translations/formatusb_tr.ts
@@ -1,6 +1,4 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.1" language="tr">
+<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="tr">
 <context>
     <name>MainWindow</name>
     <message>
@@ -150,7 +148,6 @@
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="180"/>
-        <location filename="../mainwindow.cpp" line="308"/>
         <source>Failure</source>
         <translation>Başarısız</translation>
     </message>
@@ -213,18 +210,13 @@
         <source>%1 Help</source>
         <translation>%1 Yardım</translation>
     </message>
-    <message>
-        <location filename="../mainwindow.cpp" line="308"/>
-        <source>Invalid Name</source>
-        <translation type="unfinished"></translation>
-    </message>
 </context>
 <context>
     <name>QObject</name>
     <message>
         <location filename="../about.cpp" line="34"/>
         <source>License</source>
-        <translation>Ruhsat</translation>
+        <translation>Lisans</translation>
     </message>
     <message>
         <location filename="../about.cpp" line="35"/>
