05a5a7aa2af865cebb0c29fb3a23a2c9488d11ef
Adrian <adrian@mxlinux.org>
2026-06-02 00:30:45 -0400

    New build

---
+++ debs/formatusb_26.06.dsc
@@ -0,0 +1,34 @@
+-----BEGIN PGP SIGNED MESSAGE-----
+Hash: SHA512
+
+Format: 3.0 (native)
+Source: formatusb
+Binary: formatusb
+Architecture: any
+Version: 26.06
+Maintainer: Dolphin Oracle <dolphinoracle@gmail.com>
+Homepage: https://github.com/DolphinOracle/formatusb
+Standards-Version: 4.5.1
+Vcs-Browser: https://github.com/DolphinOracle/formatusb
+Vcs-Git: https://github.com/DolphinOracle/formatusb.git
+Build-Depends: debhelper-compat (= 13), qt6-base-dev, qt6-base-dev-tools, qt6-tools-dev, qt6-tools-dev-tools
+Package-List:
+ formatusb deb admin optional arch=any
+Checksums-Sha1:
+ 2f29e888c7d6ecbe14c1411bb992a43404651c4c 214004 formatusb_26.06.tar.xz
+Checksums-Sha256:
+ 9ae6c0aa7ae879412500f993a072d02a1782d2c60fae2a7e055fb2a0cc90925f 214004 formatusb_26.06.tar.xz
+Files:
+ 72b098f263f1d190b777e1d7ed0be78f 214004 formatusb_26.06.tar.xz
+
+-----BEGIN PGP SIGNATURE-----
+
+iQFHBAEBCgAxFiEE8ndToY6S45N+YzXncJOMeAZ57pgFAmoeXG8THGFkcmlhbkBt
+eGxpbnV4Lm9yZwAKCRBwk4x4BnnumLW6CAC4ULVbNg5zlJDJBHsuvZc6dIUpZQul
+dWCzj614sz6IJgGjhYhjDm8HRVPexSYysUxdy1sNODCW3Hv5c0oMfqIrx58PFby9
+OJm2gk0jIJmL/gCfmM8ZZwWbSRoL27yHYYXo1IuePeJKWGYez8UO8kwPZEBMhSbr
+jB4sR9NRbGoDrPYWZHXOzPxyjtsITx8tgEDo1PILqLjJLMvWwclKAw8DUB5se7C+
+Ep7BISBvWCQoSg492CcWkVjxEOsM9WQT5FLHH0TJqoWg0za6FlzXANPPNdBFeFUN
+KXV7fwNikt9RZQSenIRIOqhbO6eXkvIptJ3wtffOy/qT/84PqW/rStw2
+=oqY4
+-----END PGP SIGNATURE-----
---
+++ translations/formatusb_ko.ts
@@ -0,0 +1,222 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="ko">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.ui" line="14"/>
+        <source>Program_Name</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="39"/>
+        <source>Show all devices</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="46"/>
+        <source>Format</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="54"/>
+        <source>Defaults</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="59"/>
+        <source>msdos</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="64"/>
+        <source>gpt</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="72"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Select Target USB Device&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="82"/>
+        <source>Show partitions</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="138"/>
+        <source>Refresh drive list</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="152"/>
+        <source>USB-DATA</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="159"/>
+        <source>File System Label</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="166"/>
+        <source>Partition Table Type</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="225"/>
+        <source>Quit application</source>
+        <translation>종료</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="228"/>
+        <source>Close</source>
+        <translation>닫기</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="235"/>
+        <source>Alt+N</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="273"/>
+        <source>Display help </source>
+        <translation>도움말 표시</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="276"/>
+        <source>Help</source>
+        <translation>도움말</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="283"/>
+        <source>Alt+H</source>
+        <translation>Alt+H</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="315"/>
+        <source>About this application</source>
+        <translation>이 애플리케이션 정보</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="318"/>
+        <source>About...</source>
+        <translation>About...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="325"/>
+        <source>Alt+B</source>
+        <translation>Alt+B</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="357"/>
+        <source>Next</source>
+        <translation>다음으로</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="383"/>
+        <source>Back</source>
+        <translation>뒤로</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="184"/>
+        <source>Success</source>
+        <translation>성공</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="184"/>
+        <source>Format successful!</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="186"/>
+        <location filename="../mainwindow.cpp" line="315"/>
+        <source>Failure</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="186"/>
+        <source>Error encountered in the Format process</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="217"/>
+        <source>Error</source>
+        <translation>에러</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="217"/>
+        <source>Please select a USB device to write to</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="222"/>
+        <source>These actions will destroy all data on 
+
+</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="222"/>
+        <source>Do you wish to continue?</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="258"/>
+        <source>About %1</source>
+        <translation>%1 정보</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="259"/>
+        <source>Version: </source>
+        <translation>버전:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="260"/>
+        <source>Program for formatting USB devices</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="262"/>
+        <source>Copyright (c) MX Linux</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="263"/>
+        <source>%1 License</source>
+        <translation>%1 라이센스</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="271"/>
+        <source>%1 Help</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="315"/>
+        <source>Invalid Name</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../about.cpp" line="34"/>
+        <source>License</source>
+        <translation>라이센스</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="35"/>
+        <location filename="../about.cpp" line="45"/>
+        <source>Changelog</source>
+        <translation>변경 로그</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="36"/>
+        <source>Cancel</source>
+        <translation>취소</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="53"/>
+        <source>&amp;Close</source>
+        <translation>닫기(&amp;C)</translation>
+    </message>
+</context>
+</TS>
